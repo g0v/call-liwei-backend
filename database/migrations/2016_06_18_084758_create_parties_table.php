99b71c4f871d92506a8789d66e6c79a73691e117
hong19 <jimmy600kimo@hotmail.com>
2016-06-18 16:48:31 +0800

    feat(mirgation): create Parties table

---
+++ database/migrations/2016_06_18_084758_create_parties_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePartiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('parties', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+
+            $table->string('name', 60);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('parties');
+    }
+}
