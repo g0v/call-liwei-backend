1c7df15370e7e0e113fe23e89b1ea5c5d5fd5d89
hong19 <jimmy600kimo@hotmail.com>
2016-06-18 17:14:01 +0800

    feat(migration): create districts table

---
+++ database/migrations/2016_06_18_085628_create_districts_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDistrictsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('districts', function (Blueprint $table) {
+            $table->increments('id');
+            $table->timestamps();
+            $table->string('name', 60);
+            $table->integer('electoral_district_id')->unsigned();
+
+            $table->foreign('electoral_district_id')->references('id')->on('electoral_districts');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('districts');
+    }
+}
